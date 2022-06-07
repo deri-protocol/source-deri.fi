cc65241a654ae3bf546f378d7268a3ea3626fe7f
deri-protocol <deri-protocol@dfactory.tech>
2022-06-07 18:09:25 +0800

    export componets from common

--- common/src/index.js
+++ common/src/index.js
@@ -3,11 +3,18 @@ import WalletConnector from './components/WalletConnector/WalletConnector'
 import ChainSelector from './components/ChainSelector/ChainSelector'
 import * as utils from './utils/utils'
 import * as Constants from './utils/Constants'
-
+import Button from './components/Button/Button'
+import Tooltip from './components/Tooltip/Tooltip'
+import Loading from './components/Loading/Loading'
+import UnderlineText from './components/UnderlineText/UnderlineText'
 export {
   Icon,
   WalletConnector,
   ChainSelector,
   utils,
-  Constants
+  Constants,
+  Button,
+  Tooltip,
+  Loading,
+  UnderlineText
 }

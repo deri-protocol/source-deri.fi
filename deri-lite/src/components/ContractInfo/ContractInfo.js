2c35493c3fa85ae4ccff81bdc642f673b264fa6d
jack <>
2022-06-09 10:06:45 +0800

    fix

--- deri-lite/src/components/ContractInfo/ContractInfo.js
+++ deri-lite/src/components/ContractInfo/ContractInfo.js
@@ -3,11 +3,11 @@ import NumberFormat from 'react-number-format'
 import { inject, observer } from 'mobx-react';
 import TipWrapper from '../TipWrapper/TipWrapper';
 import version from '../../model/Version';
-import {DeriEnv} from '../../lib/web3js/indexV2'
+import { DeriEnv } from '../../lib/web3js/indexV2'
 
 function ContractInfo({ wallet, trading, lang, type }) {
 
-  const toNonExponential = (num)=>{
+  const toNonExponential = (num) => {
     num = +(num.toFixed(11))
     var m = num.toExponential().match(/\d(?:\.(\d*))?e([+-]\d+)/);
     return m[0];
@@ -19,50 +19,32 @@ function ContractInfo({ wallet, trading, lang, type }) {
       <div className="contract-info">
         <div className="conntract-header">{lang['contract-info']}</div>
         <div className="info">
-          <div className="title">{lang['base-token']}</div>
-            <div className="text" >
-              {trading.contract.bTokenSymbol && trading.contract.bTokenSymbol.map((bToken,index) => {
-                return ( (version.isV2 && type.isFuture )
-                  ? <TipWrapper key={index}><span className='btoken-symbol'>{bToken}(<span className='multiplier' tip={lang['multiplier-tip']}>{trading.contract.bTokenMultiplier && trading.contract.bTokenMultiplier[index]}x</span>)</span></TipWrapper>
-                  : <span key={index} className='btoken-symbol'>{bToken}</span>)  
-              })}
-            </div>
+          <div className="title">Base Token
+            <TipWrapper>
+              <span tip="Discount Factor is the effective ratio of the margin token counted as collateral (converted into dynamic effective balance). For example: posting 10 USD in CAKE token (with DiscountFactor = 0.685) to your margin account will increase your dynamic effective margin by 6.85 USD.">(Discount Factor)</span>
+            </TipWrapper>
+          </div>
+          <div className="text" >
+            {trading.contract.bTokenSymbol && trading.contract.bTokenSymbol.map((bToken, index) => {
+              return (<span key={index} className='btoken-symbol'>{bToken}</span>)
+            })}
+          </div>
         </div>
         <div className="info">
           <div className="title">
-            {type.isOption && lang['symbol-option']}
-            {type.isFuture && lang['symbol']} 
+            {lang['symbol']}
           </div>
           <div className="text">
             {trading.contract.symbol}
           </div>
         </div>
-
         {type.isFuture && <>
           <div className="info">
-            <div className="title"> <span>{lang['min-trade-volume']} ( {lang['notional']} )</span> </div>
+            <div className="title"> <span>Min.Trade Unit (Notional)</span> </div>
             <div className="text">
               {trading.contract.multiplier} {trading.config ? trading.config.unit : ''}
             </div>
           </div>
-          { (version.isOpen || version.isV1) && 
-          <div className="info">
-            <div className="title">{lang['funding-rate-coefficient']}</div>
-            <div className="text">
-              {trading.contract.fundingRateCoefficient}
-            </div>
-          </div>
-          }
-          {!version.isOpen && 
-            <div className="info">
-            <div className="title">{lang['funding-rate-coefficient']}</div>
-            <div className="text">
-              <TipWrapper block={false} >
-                <span tip={trading.fundingCoefficientTip} className='margin-per'>{trading.contract.fundingRateCoefficient ? toNonExponential(+trading.contract.fundingRateCoefficient) :'' }</span> 
-              </TipWrapper>
-            </div>
-          </div>
-          }
           <div className="info">
             <div className="title">{lang['min-initial-margin-ratio']}</div>
             <div className="text">
@@ -96,7 +78,7 @@ function ContractInfo({ wallet, trading, lang, type }) {
             </div>
           </div>
           <div className="info">
-            <div className="title"> <span>{lang['min-trade-volume']} ( {lang['notional']} )</span> </div>
+            <div className="title"> <span> Min.Trade Unit (Notional)</span> </div>
             <div className="text">
               {trading.contract.multiplier} {trading.config ? trading.config.unit : ''}
             </div>
@@ -121,7 +103,7 @@ function ContractInfo({ wallet, trading, lang, type }) {
           {type.isOption && <>
             <div className="title">
               <TipWrapper block={false}>
-                <span className="margin-per" tip={trading.TransactionFeeTip}>{lang['transaction-fee']}</span>  
+                <span className="margin-per" tip={trading.TransactionFeeTip}>{lang['transaction-fee']}</span>
               </TipWrapper>
             </div>
           </>}

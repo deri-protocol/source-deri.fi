ad45fe0d192b06b1ddb7d4241cff99f3d1955200
BraunsenLX <deri-protocol@dfactory.tech>
2022-06-09 19:15:17 +0800

    make it to deri fi

--- deri-lite/config/webpack.config.js
+++ deri-lite/config/webpack.config.js
@@ -200,36 +200,41 @@ module.exports = function (webpackEnv) {
       // The build folder.
       path: isEnvProduction ? paths.appBuild : undefined,
       // Add /* filename */ comments to generated require()s in the output.
-      pathinfo: isEnvDevelopment,
+      // pathinfo: isEnvDevelopment,
       // There will be one main bundle, and one file per asynchronous chunk.
       // In development, it does not produce real files.
       filename: isEnvProduction
         ? 'static/js/[name].[contenthash:8].js'
-        : isEnvDevelopment && 'static/js/bundle.js',
-      // TODO: remove this when upgrading to webpack 5
-      futureEmitAssets: true,
+        : isEnvDevelopment && `${appPackageJson.name}.js`,
+      // // TODO: remove this when upgrading to webpack 5
+      // futureEmitAssets: true,
       // There are also additional JS chunk files if you use code splitting.
-      chunkFilename: isEnvProduction
-        ? 'static/js/[name].[contenthash:8].chunk.js'
-        : isEnvDevelopment && 'static/js/[name].chunk.js',
+      // chunkFilename: isEnvProduction
+      //   ? 'static/js/[name].[contenthash:8].chunk.js'
+      //   : isEnvDevelopment && 'static/js/[name].chunk.js',
       // webpack uses `publicPath` to determine where the app is being served from.
       // It requires a trailing slash, or the file assets will get an incorrect path.
       // We inferred the "public path" (such as / or /my-project) from homepage.
-      publicPath: paths.publicUrlOrPath,
+      // publicPath: paths.publicUrlOrPath,
       // Point sourcemap entries to original disk location (format as URL on Windows)
-      devtoolModuleFilenameTemplate: isEnvProduction
-        ? info =>
-            path
-              .relative(paths.appSrc, info.absoluteResourcePath)
-              .replace(/\\/g, '/')
-        : isEnvDevelopment &&
-          (info => path.resolve(info.absoluteResourcePath).replace(/\\/g, '/')),
+      // devtoolModuleFilenameTemplate: isEnvProduction
+      //   ? info =>
+      //       path
+      //         .relative(paths.appSrc, info.absoluteResourcePath)
+      //         .replace(/\\/g, '/')
+      //   : isEnvDevelopment &&
+      //     (info => path.resolve(info.absoluteResourcePath).replace(/\\/g, '/')),
       // Prevents conflicts when multiple webpack runtimes (from different apps)
       // are used on the same page.
-      jsonpFunction: `webpackJsonp${appPackageJson.name}`,
+      jsonpFunction: `webpackJsonp_${appPackageJson.name}`,
       // this defaults to 'window', but by setting it to 'this' then
       // module chunks which are built will work in web workers as well.
       globalObject: 'this',
+
+      library : `${appPackageJson.name}`,
+      libraryTarget : 'umd',
+      publicPath : `${process.env.PUBLIC_URL}`,
+      // chunkLoadingGlobal : `webpackJsonp_${appPackageJson.name}`
     },
     optimization: {
       minimize: isEnvProduction,

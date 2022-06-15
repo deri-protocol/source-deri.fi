49008f11f80c16c79361d9025bd632b512e5ef11
Zadiel <>
2022-06-15 16:05:18 +0800

    fix

--- bet-it/src/App.js
+++ bet-it/src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './components/Header/Header';
 import BetIt from './pages/BetIt';
 import { inject, observer } from 'mobx-react';
+import { useWallet } from "use-wallet";
 import PageRouter from './pages/PageRouter';
 import { useLocation } from 'react-router-dom'
 import { DeriEnv } from './web3';
@@ -11,6 +12,10 @@ import { DeriEnv } from './web3';
 function App({intl,actions}) {
   const location = useLocation();
   const curRouterClass = location.pathname.split('/')[1]
+  const wallet = useWallet()
+  useEffect(()=>{
+    wallet.connect()
+  },[])
   return (
     <div className={`App ${curRouterClass}`}>
       <PageRouter intl={intl} actions={actions}></PageRouter>

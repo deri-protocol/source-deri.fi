c5874a8a59c0fdf68907dc83acf7a2a7d074066b
deri-protocol <deri-protocol@dfactory.tech>
2022-06-15 19:09:14 +0800

    review

--- common/src/components/WalletConnector/WalletConnector.js
+++ common/src/components/WalletConnector/WalletConnector.js
@@ -9,6 +9,7 @@ export default function WalletConnector({lang,bgColor = '#FFAB00',actions}){
   const [bntColor, setBntColor] = useState('#FFAB00');
   const [expand, setExpand] = useState();
   const wallet = useWallet()
+
   const walletClass = classNames('wallet-view',{
     expand : expand,
     shrink : expand === false,
@@ -48,6 +49,12 @@ export default function WalletConnector({lang,bgColor = '#FFAB00',actions}){
   useEffect(() => {
     actions && actions.setGlobalState({wallet : wallet})
   }, [wallet]);
+
+  useEffect(() => {
+    if(!wallet.isConnected()){
+      wallet.connect();
+    }
+  }, [location.hash]);
   
   return(
     <div className={walletClass}>

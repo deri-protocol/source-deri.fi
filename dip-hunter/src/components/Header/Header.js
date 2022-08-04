9e2c257d49795a3ccc071003c6f7b98bd4880ed4
Zadiel <76576293+huangwenchao2000@users.noreply.github.com>
2022-08-04 10:20:23 +0800

    fix

--- dip-hunter/src/components/Header/Header.js
+++ dip-hunter/src/components/Header/Header.js
@@ -1,13 +1,17 @@
 // import NetworkSelector from "../NetworkSelector/NetworkSelector";
 import { Icon, ChainSelector, WalletConnector } from '@deri/eco-common';
 import styled from 'styled-components';
+import { useCallback, useEffect } from 'react';
+import classNames from "classnames";
+import { useState } from 'react';
+import { isStartScroll } from "../../utils/utils";
 
 const Wrapper = styled.div`
   &.header {
     position : initial;
     font-size :32px;
-    display : none;
-    background-color : #FF891D;
+    display:none;
+    background : none;
     justify-content: space-between;
     align-items: center;
     height : 88px;
@@ -87,28 +91,123 @@ const Wrapper = styled.div`
       }
     }
   }
+
+  @media only screen and (min-width : 761px ){
+    &.header {
+      position : initial;
+      font-size :32px;
+      display : flex;
+      padding : 0 110px;
+      background : none;
+      justify-content: space-between;
+      align-items: center;
+      height : 96px;
+      .chain-wallet-btn{
+        display:none;
+      }
+      .left-menu {
+        display : none;
+      }
+      .f-name {
+        font-weight: 800;
+        color : #FFAB00; 
+        margin-right : 4px;
+      }
+      .l-name {
+        font-weight: 800;
+        color : #FFF;
+        margin-right :140px;
+      }
+      .right {
+        display : flex;
+      }
+    }
+    &.header.fixed {
+      position : fixed;
+      top : 0px;
+      left : 0;
+      box-sizing: border-box;
+      width : 100%;
+      justify-content: space-between;
+      background : #FFAB00;
+      z-index : 10; 
+      animation : headerFadein 1s ease; 
+      .chain-wallet-btn{
+        display:flex;
+      }
+      .f-name {
+        color : #FF7913;
+      }
+    }
+    &.header.fadeOut {
+      animation : headerFadeout 0.5s ease; 
+      display:none;
+    }
+  }
+  
+@keyframes headerFadein {
+  from {
+    // opacity: 0;
+    height : 0px;
+  }
+  to {
+    // opacity: 1;
+    height : 96px;
+  }
+}
+@keyframes headerFadeout {
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+}
   
 
 `
 
 export default function Header({ lang, collect, switchMenu }) {
+  const [isFixed, setIsFixed] = useState(false);
+  const [btnMainColor, setBtnMainColor] = useState('#FFAB00');
+  const clazz = classNames('header', {
+    fixed: isFixed,
+    fadeOut: !isFixed
+  })
+  const handler = useCallback(() => {
+    let offset = collect ? 138 : 202
+    if (isStartScroll(offset)) {
+      setIsFixed(true)
+      setBtnMainColor('#FF7913')
+    } else {
+      setIsFixed(false)
+      setBtnMainColor('#FFAB00')
+    }
+  })
+
+  useEffect(() => {
+    document.addEventListener('scroll', handler, false);
+    return () => {
+      document.removeEventListener('scroll', handler)
+    }
+  }, [collect])
   return (
-    <Wrapper className="header">
+    <Wrapper className={clazz}>
       <div className="left">
         <div className="logo-title">
           <span className='left-menu' onClick={switchMenu}><Icon token='left-menu' width='22.5' /></span>
           <span className="f-name">Dip</span><span className="l-name">Hunter</span>
         </div>
-        <div className="helps-info">
+        {!isFixed &&<div className="helps-info">
           {lang["info-des"]} <span>
             {lang["passive-income"]}
           </span>
-        </div>
+        </div>}
       </div>
       <div className="right">
         <div className='chain-wallet-btn'>
-          <ChainSelector bgColor="#FFAB00" collect={collect} id="header-network" />
-          <WalletConnector lang={lang} bgColor="#FFAB00" />
+          <ChainSelector bgColor={btnMainColor} collect={collect} id="header-network" />
+          <WalletConnector lang={lang} bgColor={btnMainColor} />
         </div>
       </div>
 
